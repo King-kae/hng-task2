c631f29371013e27f080a984c8e8c6492adc6eab
Kayode Raimi <kayode.raimi123@gmail.com>
2024-07-07 15:24:32 +0100

    commit

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,48 +1,14 @@
 const { Sequelize } = require('sequelize');
-const { PostgresDialect } = require('@sequelize/postgres')
-const CONFIG = require('../config/config.js');
-const {pg} = require('pg');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectModule: pg
+const sequelize = new Sequelize(process.env.DB_LINK, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true,
+    native:true
   }
-)
+});
 
-// sequelize.authenticate()
-//   .then(() => console.log('Connection has been established successfully.'))
-//   .catch(err => console.error('Unable to connect to the database:', err));
 
-const createDatabase = async () => {
-  const { Client } = require('pg');
-  const client = new Client({
-    user: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    port: 5432,
-  });
-
-  try {
-    await client.connect();
-    await client.query('CREATE DATABASE "database"');
-    console.log('Database created successfully');
-  } catch (error) {
-    if (error.code !== '42P04') {
-      console.error('Failed to create database', error);
-      process.exit(1);
-    } else {
-      console.log('Database already exists');
-    }
-  } finally {
-    await client.end();
-  }
-};
-
-module.exports = { sequelize, createDatabase };
-
+module.exports = { sequelize };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const authRouter = require('./routes/user.route.js');
 const orgRouter = require('./routes/org.route.js');
 const { isAuthenticated } = require('./middleware/auth.js')
 // const { connectToMongoDB } = require('./db/db.js');
-const { sequelize, createDatabase } = require('./db/dbConnect.js')
+const { sequelize } = require('./db/dbConnect.js')
 const { syncModels } = require('./models/index.js')
 const app = express();
 const PORT = 5000
@@ -24,9 +24,8 @@ app.use('/', authRouter)
 
 const startServer = async () => {
     try {
-      await createDatabase();
-      await sequelize.authenticate();
-      await syncModels();
+      await sequelize.authenticate();  // Authenticate connection
+      await syncModels();  // Sync models
       app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
       });
