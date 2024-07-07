254ba3ef958f29b9928dfd7dd0102443d4499c13
Kayode Raimi <kayode.raimi123@gmail.com>
2024-07-07 10:55:06 +0100

    third commit

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,44 +1,46 @@
 const { Sequelize } = require('sequelize');
+const { PostgresDialect } = require('@sequelize/postgres')
 const CONFIG = require('../config/config.js');
+require('dotenv').config();
 
 const sequelize = new Sequelize(
-    CONFIG.DB_NAME,
-    CONFIG.DB_USER, 
-    CONFIG.DB_PASSWORD,
-    {
-        host: CONFIG.DB_HOST,
-        dialect: CONFIG.DB_DIALECT
-    }
+  {
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: PostgresDialect
+  }
 )
 
 // sequelize.authenticate()
 //   .then(() => console.log('Connection has been established successfully.'))
 //   .catch(err => console.error('Unable to connect to the database:', err));
 
-  const createDatabase = async () => {
-    const { Client } = require('pg');
-    const client = new Client({
-      user: CONFIG.DB_NAME,
-      host: CONFIG.DB_HOST,
-      password: CONFIG.DB_PASSWORD,
-      port: 5432,
-    });
-  
-    try {
-      await client.connect();
-      await client.query('CREATE DATABASE "database"');
-      console.log('Database created successfully');
-    } catch (error) {
-      if (error.code !== '42P04') {
-        console.error('Failed to create database', error);
-        process.exit(1);
-      } else {
-        console.log('Database already exists');
-      }
-    } finally {
-      await client.end();
+const createDatabase = async () => {
+  const { Client } = require('pg');
+  const client = new Client({
+    user: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+  });
+
+  try {
+    await client.connect();
+    await client.query('CREATE DATABASE "database"');
+    console.log('Database created successfully');
+  } catch (error) {
+    if (error.code !== '42P04') {
+      console.error('Failed to create database', error);
+      process.exit(1);
+    } else {
+      console.log('Database already exists');
     }
-  };
-  
-  module.exports = { sequelize, createDatabase };
+  } finally {
+    await client.end();
+  }
+};
+
+module.exports = { sequelize, createDatabase };
 
